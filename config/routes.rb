fe9aa1d85bbb17db8bec93020789947d631576f2
ronniekram <arynn.boniface@gmail.com>
2020-08-08 16:41:50 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :villagers
-  resources :wishlists
-  resources :islands
+  resources :villagers, except: [:update]
+  resources :wishlists, only: [:create, :show, :update, :destroy]
+  resources :islands, only: [:create, :show, :update]
 end
