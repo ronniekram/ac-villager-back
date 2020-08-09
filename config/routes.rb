cb039e8763ac857099a2bbf2b1ca24e389fc7095
ronniekram <arynn.boniface@gmail.com>
2020-08-08 18:36:32 -0700

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :villagers, except: [:update]
-  resources :wishlists, only: [:index, :create, :show, :destroy]
-  resources :islands, only: [:index, :create, :show]
+  resources :villagers
+
+  resources :wishlists do 
+    resources :villagers
+  end 
+
+  resources :islands do 
+    resources :villagers
+  end 
+
 end
