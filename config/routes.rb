6c6dcf09d139fa7a2cf20023f661e87bd43fbde0
ronniekram <arynn.boniface@gmail.com>
2020-08-12 20:11:12 -0700

    fixed some minor migration issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :villagers
 
-  resources :islands 
+  resources :islands do 
+    resources :villagers
+  end 
 
-  resources :wishlists
+  resources :wishlists do 
+    resources :villagers
+  end
 
 end
