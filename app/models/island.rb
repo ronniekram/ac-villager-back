fe7213a6886ede30ae718b8a583de3b484344bd1
ronniekram <arynn.boniface@gmail.com>
2020-08-12 21:27:24 -0700

    added wishlist and island validations

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,5 +1,6 @@
 class Island < ApplicationRecord
-  validates :name, :owner, presence: true
+  validates :name, :owner, presence: true, uniqueness: true
   validates :villager_ids, length: { maximum: 10 }
+  validates :villager_ids, uniquenes: true
   has_many :villagers, :dependent => :destroy
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,5 @@
 class Wishlist < ApplicationRecord
-  validates :name, :owner, presence: true
+  validates :name, :owner, presence: true, uniqueness: true
+  validates :villager_ids, uniquenes: true
   has_many :villagers, :dependent => :destroy
 end
