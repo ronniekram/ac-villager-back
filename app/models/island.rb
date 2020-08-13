f30275fe50886df28b52889495e0c9640cdd56ff
ronniekram <arynn.boniface@gmail.com>
2020-08-13 12:02:30 -0700

    adjusted island and wishlist model

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,6 +1,5 @@
 class Island < ApplicationRecord
   validates :name, :owner, presence: true, uniqueness: true
   validates :villager_ids, length: { maximum: 10 }
-  validates :villager_ids, uniquenes: true
   has_many :villagers, :dependent => :destroy
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,5 +1,4 @@
 class Wishlist < ApplicationRecord
   validates :name, :owner, presence: true, uniqueness: true
-  validates :villager_ids, uniquenes: true
   has_many :villagers, :dependent => :destroy
 end
