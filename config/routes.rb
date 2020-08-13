b8b1eb2058d212e65792d92ca211b4f0435127d1
ronniekram <arynn.boniface@gmail.com>
2020-08-13 14:54:43 -0700

    unnested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :villagers
 
-  resources :islands do 
-    resources: villagers
-  end 
+  resources :islands
 
-    resources :wishlists do 
-    resources: villagers
-  end
+  resources :wishlists
 end
