692caa7dcb41c932375660f3c76393c4bd50067a
ronniekram <arynn.boniface@gmail.com>
2020-08-12 23:48:56 -0700

    fixed routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :villagers
-
-  resources :islands do 
-    resources :villagers
-  end 
-
-  resources :wishlists do 
-    resources :villagers
-  end
-
+  resources :islands
+  resources :wishlists
 end
